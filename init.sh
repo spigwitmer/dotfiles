560a26f17d00d0046af3f967276ec36d3a81b83c
spigwitmer <itgpmc@gmail.com>
2016-02-28 05:16:01 -0800

    s/home-cfg/dotfiles/

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ if [ -x _terraform.sh ]; then
     ./_terraform.sh
 else
     TMPDIR="`mktemp -d`"
-    git clone https://github.com/spigwitmer/home-cfg.git $TMPDIR
+    git clone https://github.com/spigwitmer/dotfiles.git $TMPDIR
     $TMPDIR/_terraform.sh $@
 fi
